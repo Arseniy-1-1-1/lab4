07d8869ee92a68f3e70ab194f7b67e04186107e8
Arseniy-1-1-1 <yusipov2006@gmail.com>
2026-03-20 23:40:52 +0300

    lab44

--- PolynomP/tests/tests.cpp
+++ PolynomP/tests/tests.cpp
@@ -5,27 +5,28 @@
 #include <iostream>
 
 struct Monom {
-	double coeff;
-	int degree;
-	Monom* next;
+  double coeff;
+  int degree;
+  Monom *next;
 };
 
 class Polynom {
-	Monom* head;
-	int getX(int deg) const;
-	int getY(int deg) const;
-	int getZ(int deg) const;
-	int makeDegree(int x, int y, int z) const;
+  Monom *head;
+  int getX(int deg) const;
+  int getY(int deg) const;
+  int getZ(int deg) const;
+  int makeDegree(int x, int y, int z) const;
+
 public:
-	Polynom();
-	~Polynom();
-	void addMonom(double coef, int degree);
-	Polynom operator+(const Polynom& other) const;
-	Polynom operator-(const Polynom& other) const;
-	Polynom operator*(double sc) const;
-	Polynom operator*(const Polynom& other) const;
-	void print() const;
-	bool operator==(const Polynom& other) const;
+  Polynom();
+  ~Polynom();
+  void addMonom(double coef, int degree);
+  Polynom operator+(const Polynom &other) const;
+  Polynom operator-(const Polynom &other) const;
+  Polynom operator*(double sc) const;
+  Polynom operator*(const Polynom &other) const;
+  void print() const;
+  bool operator==(const Polynom &other) const;
 };
 
 #endif
