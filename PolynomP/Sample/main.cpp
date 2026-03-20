5ce61c0298dddf66b76edd4c014eda51b46e4fcd
Arseniy-1-1-1 <yusipov2006@gmail.com>
2026-03-20 23:33:56 +0300

    lab44

--- PolynomP/Sample/main.cpp
+++ PolynomP/Sample/main.cpp
@@ -1,35 +1,36 @@
-#include "Polynom.h"
 #include <stdio.h>
+
 #include <iostream>
 
+#include "Polynom.h"
 
 Polynom inputPoly() {
-	Polynom p;
-	int n;
-	std::cout << "monom count: ";
-	std::cin >> n;
+  Polynom p;
+  int n;
+  std::cout << "monom count: ";
+  std::cin >> n;
 
-	std::cout << "coeff x y z\n";
+  std::cout << "coeff x y z\n";
 
-	for (int i = 0; i < n; i++) {
-		double c;
-		int x, y, z;
-		std::cin >> c >> x >> y >> z;
-		p.addMonom(c, x * 100 + y * 10 + z);
-	}
+  for (int i = 0; i < n; i++) {
+    double c;
+    int x, y, z;
+    std::cin >> c >> x >> y >> z;
+    p.addMonom(c, x * 100 + y * 10 + z);
+  }
 
-	return p;
+  return p;
 }
 
 int main() {
-	std::cout << "Hello\n";
-	std::cout << "A:\n";
-	Polynom A = inputPoly();
-	std::cout << "B:\n";
-	Polynom B = inputPoly();
-	std::cout << "A+B:\n";
-	Polynom C = A + B;
-	C.print();
+  std::cout << "Hello\n";
+  std::cout << "A:\n";
+  Polynom A = inputPoly();
+  std::cout << "B:\n";
+  Polynom B = inputPoly();
+  std::cout << "A+B:\n";
+  Polynom C = A + B;
+  C.print();
 
-	return 0;
+  return 0;
 }
